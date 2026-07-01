202fb81bc004540e8a3c7ded018e5a8b07a7ca33
rimabazi9-create <rimabazi9@gmail.com>
2026-07-01 06:37:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'success', message: 'JSON Viewer is working!' });
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
